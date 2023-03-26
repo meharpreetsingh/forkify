9b998e634ea1f1023a9a866d069f418e87ec2d0f
Meharpreet Singh <meharpreet0000@gmail.com>
2023-03-27 02:23:15 +0530

    Fraction Package Workaround

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -127,15 +127,14 @@ class RecipeView extends View {
           </a>
         </div>`;
   }
+  // ing.quantity ? new Fraction(ing.quantity).toString() : ''
   _generateMarkupIngreient(ing) {
     return `
   <li class="recipe__ingredient">
   <svg class="recipe__icon">
     <use href="${icons}#icon-check"></use>
   </svg>
-  <div class="recipe__quantity">${
-    ing.quantity ? new Fraction(ing.quantity).toString() : ''
-  }</div>
+  <div class="recipe__quantity">${ing.quantity ? ing.quantity : ''}</div>
   <div class="recipe__description">
     <span class="recipe__unit">
     ${ing.unit}</span>
